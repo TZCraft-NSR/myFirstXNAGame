a2259c5859d0c3a7abfbf7cda374f604dcb6b812
Nire Inicana <thakyZ@users.noreply.github.com>
2014-12-03 10:14:04 -0800

    Update Pong.cs

--- myFirstXNAGame/myFirstXNAGame/Pong.cs
+++ myFirstXNAGame/myFirstXNAGame/Pong.cs
@@ -13,43 +13,84 @@ using VoidEngine;
 
 namespace myFirstXNAGame
 {
+    /// <summary>
+    /// The Pong class
+    /// </summary>
     class Pong : Sprite
     {
-        protected bool start = true;
+        myFirstXNAGame myGame;
+        Vector2 direction;
 
-        public Pong(Vector2 position) : base(position)
+        /// <summary>
+        /// Creates the Pong from the class.
+        /// </summary>
+        /// <param name="position">The position of the pong</param>
+        public Pong(Texture2D texture, Vector2 position, myFirstXNAGame myGame) : base(position)
         {
+            AddAnimations(texture);
+            speed = 2;
+            this.position = position;
+            this.myGame = myGame;
+
+            direction.X = (2 * (float)myGame.rng.NextDouble() - 1);
+            direction.Y = (2 * (float)myGame.rng.NextDouble() - 1);
         }
 
-        public override void Update(GameTime gameTime, Vector2 left, Vector2 right, Vector2 leftSize, Vector2 rightSize)
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
         {
-            if (start == true)
+            if (keyboardState.IsKeyDown(Keys.W))
             {
-                position.X += 2;
-                start = false;
+                position.Y -= speed;
+            }
+            if (keyboardState.IsKeyDown(Keys.A))
+            {
+                position.X -= speed;
+            }
+            if (keyboardState.IsKeyDown(Keys.S))
+            {
+                position.Y += speed;
+            }
+            if (keyboardState.IsKeyDown(Keys.D))
+            {
+                position.X += speed;
             }
 
-            if (position.Y <= left.Y && position.Y >= leftSize.Y)
+            direction = Collision.UnitVector(direction);
+
+            direction.X = direction.X * speed;
+            direction.Y = direction.Y * speed;
+
+            foreach (Collision.MapSegment ms in myGame.MapSegments)
             {
-                if (position.X <= left.X + leftSize.X)
+                if (collisionRect().Intersects(ms.collisionRect()))
                 {
-                    position.X += 2;
-                }
-            }
-            if (position.Y >= right.Y && position.Y >= rightSize.Y)
-            {
-                if (position.X <= right.X + rightSize.X)
-                {
-                    position.X -= 2;
+                    speed = 0;
                 }
             }
 
             base.Update(gameTime);
         }
 
+        /// <summary>
+        /// Put inbetween the spriteBatch.Begin and spriteBatch.End
+        /// </summary>
+        /// <param name="gameTime">The main GameTime</param>
+        /// <param name="spriteBatch">The main SpriteBatch</param>
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             base.Draw(gameTime, spriteBatch);
+        }
+
+        public override void AddAnimations(Texture2D texture)
+        {
+            Addanimation("IDLE", texture, new Point(40, 40), new Point(0, 0), new Point(0, 0), 1000);
+            SetAnimation("IDLE");
+            base.AddAnimations(texture);
         }
     }
 }
